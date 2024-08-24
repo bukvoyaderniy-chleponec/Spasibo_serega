69c8ceb0731192ed6840c79870bb6426be9a5b40
bukvoyaderniy-chleponec <katyakrut2007@gmail.com>
2024-08-24 19:55:31 +0300

    TODO: rename

--- господи прости.cpp
+++ господи прости.cpp
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<txlib.h>
-#include<math.h>
-#include<assert.h>
+#include <stdio.h>
+#include <txlib.h>
+#include <math.h>
+#include <assert.h>
 
 
-struct Pekusi {
-    int n1test;
+struct Pekusi {// TODO: rename all
+    int n1test;// TODO: rename all
     double a, b, c;
-    double x1banal, x2banal;
+    double x1banal, x2banal;// TODO: rename all
     int nRootsbanal;
 };
 
@@ -18,15 +18,15 @@ enum Korni {
     SS_INF_ROOTS = -1
 };
 
-const double poltarachka_v_edinichke = 0.001;
+const double poltarachka_v_edinichke = 0.001;// TODO: rename all
+
 int choose (double* a, double* b, double* c);
 int RGB_test();
-int SolveSquare (double a, double b, double c, double* x1, double* x2);
-int input (double* a, double* b, double* c);
-int RTRTRTRTRTRTRTRTRT (int counter, Pekusi data);
-int switch_case (double a, double b, double c);
-int linesolv(double* a, double* b, double* c, double* x1);
-
+int SolveSquare (double a, double b, double c, double* x1, double* x2);// TODO: rename all
+int input (double* a, double* b, double* c);// good name
+int RTRTRTRTRTRTRTRTRT (int counter, Pekusi data);   // TODO: rename all
+int switch_case  (double a, double b, double c); // TODO: rename all
+int linear_solve (double* a, double* b, double* c, double* x1);
 
 int main() {
     txSetConsoleAttr (FOREGROUND_BLACK | BACKGROUND_LIGHTBLUE);
@@ -34,7 +34,8 @@ int main() {
 
     printf ("Для проверки тестов введите 0\n");
 
-    double a = 0, b = 0, c = 0;
+    printf ("Для завершения программы введите +\n");
+
     choose (&a, &b, &c);
 }
 
@@ -43,7 +44,7 @@ int SolveSquare (double a, double b, double c, double* x1, double* x2) {
     assert (x1 != NULL);
     assert (x2 != NULL);
 
-    if (fabs (a) < poltarachka_v_edinichke) {
+    if (fabs (a) < poltarachka_v_edinichke) {    // TODO: func to compare double and 0
         if (fabs (b) < poltarachka_v_edinichke) {
            return (fabs (c) < poltarachka_v_edinichke) ? SS_INF_ROOTS : NO_ROOTS;
         }
@@ -67,7 +68,7 @@ int SolveSquare (double a, double b, double c, double* x1, double* x2) {
             *x1 = (-b - sqrt_d) / (2 * a);
             *x2 = (-b + sqrt_d) / (2 * a);
 
-            assert (fabs (x1 - x2) > poltarachka_v_edinichke);
+            assert (fabs (x1 - x2) > poltarachka_v_edinichke); // TODO: try to make less complicated
 
             return TWO_ROOTS;
         }
@@ -94,7 +95,7 @@ int RTRTRTRTRTRTRTRTRT (int counter, Pekusi data) {
     double x1 = 0, x2 = 0;
     int nRoots = SolveSquare (data.a, data.b, data.c, &x1, &x2);
 
-    if (nRoots != data.nRootsbanal|| x1 != data.x1banal || x2 != data.x2banal) {
+    if (nRoots != data.nRootsbanal || x1 != data.x1banal || x2 != data.x2banal) {
         txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_RED);
         printf ("Error test %d, a = %lg, b = %lg, c = %lg,"
                "Получили: x1 = %lg, x2 = %lg, nRoots = %d\n"
@@ -113,7 +114,7 @@ int RTRTRTRTRTRTRTRTRT (int counter, Pekusi data) {
 
 int RGB_test() {
     const int tests = 6;
-    Pekusi data [tests] = {
+    Pekusi data[tests] = {
     {1, 1, 0, -4, -2, 2, TWO_ROOTS},
     {.n1test = 2, .a = 1, .b = 0, .c = -4, .x1banal = -2, .x2banal = 2, .nRootsbanal = TWO_ROOTS},
     {3, 5, 6, 7, 0, 0, NO_ROOTS},
@@ -124,65 +125,64 @@ int RGB_test() {
     int counter = 0;
     for (int i = 0; i < tests; i++)
         counter += RTRTRTRTRTRTRTRTRT (i, data[i]);  //////////////////////
-
     return counter;
 }
 
-int choose (double* a, double* b, double* c) {
+int choose (double* a, double* b, double* c) {  // TODO: remove a, b, c
     char ch = getchar();
-    if (ch == '1') {      //TODO: switch case
-        printf ("\n");
-
-        printf ("вот сюда\n");
-        double a = 0, b = 0, c = 0;
-        input (&a, &b, &c);
-        switch_case(a,b,c);
-
-    }
-
-    if (ch == '0') {
-        int counter = RGB_test();
-
-        if (counter == 6) {
-            txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
-        };
-        if (counter < 6) {
-            txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_RED);
+    switch (ch) {
+        case '0': {
+            int counter = RGB_test();
+            if (counter == 6) {
+                txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
+            };
+            if (counter < 6) {
+                txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_RED);
+            };
+            printf ("количество верных тестов %d", counter);
         }
+        break;
 
-        printf ("количество верных тестов %d", counter);
+        case '1':
+            printf ("\n");
+            printf ("вот сюда\n");
+            double a = 0, b = 0, c = 0;
+            input (&a, &b, &c);
+            switch_case (a,b,c);// TODO: rename all
+            break;
     }
+    return 0;
 }
 
-int switch_case (double a, double b, double c) {
+int switch_case (double a, double b, double c) {// TODO: rename all
 
     double x1 = 0, x2 = 0;
 
     int nRoots = SolveSquare (a, b, c, &x1, &x2);
     switch (nRoots) {
         case NO_ROOTS:
-        txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_RED);
-        printf ("Нет решений\n");
-        return NO_ROOTS;
+            txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_RED);
+            printf ("Нет решений\n");
+            return NO_ROOTS;
 
         case ONE_ROOTS:
-        txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
-        printf ("x = %lg\n", x1);
-        return ONE_ROOTS;
+            txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
+            printf ("x = %lg\n", x1);
+            return ONE_ROOTS;
 
         case TWO_ROOTS:
-        txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
-        printf ("x1 = %lg, x2 = %lg\n", x1, x2);
-        return TWO_ROOTS;;
+            txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
+            printf ("x1 = %lg, x2 = %lg\n", x1, x2);
+            return TWO_ROOTS;;
 
         case SS_INF_ROOTS:
-        txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
-        printf ("ОООООООООооооочень много решений");
-        return SS_INF_ROOTS;
+            txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_GREEN);
+            printf ("ОООООООООооооочень много решений");
+            return SS_INF_ROOTS;
 
         default:
-        txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_RED);
-        printf (":main(): ERROR: nRoots = %d\n",nRoots);
-        break;
+            txSetConsoleAttr (FOREGROUND_YELLOW | BACKGROUND_RED);
+            printf (":main(): ERROR: nRoots = %d\n",nRoots);
+            break;
     }
 }
